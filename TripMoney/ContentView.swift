3a03a6b21bab7f42f1fce2e795eab65b1ffe0bee
LeeProgrammer <jaeho010718@gmail.com>
2022-02-26 08:07:44 +0900

    Initial Commit

---
+++ TripMoney/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TripMoney
+//
+//  Created by Lee Jaeho on 2022/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TripMoney/TripMoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripMoneyApp.swift
+//  TripMoney
+//
+//  Created by Lee Jaeho on 2022/02/26.
+//
+
+import SwiftUI
+
+@main
+struct TripMoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
